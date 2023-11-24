7db89e1ed02b0e7ab607579b831aa67c194a94ca
Ovi Thorat <151909288+Ovi-Thorat@users.noreply.github.com>
2023-11-24 13:03:25 +0530

    Add files via upload

---
+++ Addition.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+  int main ()
+{
+
+    int number1 = 21;
+    printf(" \n my number1 is = %d", number1);
+
+    int number2 = 20;
+    printf(" \n my number is =%d", number2);
+
+    int addition = 41;
+    printf(" \n addition of number1 and number2 is =%d", addition );
+
+ return 0;
+
+}
