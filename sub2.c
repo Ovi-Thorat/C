ffa683f79da2d60bcd07e4fd0c57ed94676dd65f
Ovi Thorat <151909288+Ovi-Thorat@users.noreply.github.com>
2023-11-29 18:07:37 +0530

    Add files via upload

---
+++ sub2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2, num3, sub;
+
+    printf(" enter the first value :");
+    scanf(" %d", &num1); 
+
+    printf(" enter the value :");
+    scanf(" %d", &num2);
+
+    sub= num1-num2;
+    printf(" sub of two numbers :%d", sub);
+
+
+
+
+
+    return 0;
+}
