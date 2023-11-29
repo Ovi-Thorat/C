e5321e70943f68503bf0319b303c21478ee33263
Ovi Thorat <151909288+Ovi-Thorat@users.noreply.github.com>
2023-11-29 18:12:34 +0530

    Create division.c

---
+++ division.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+double main ()
+{
+    int division;
+     double num1 = 25.487;
+     printf("\n my num1 is = %lf", num1);
+
+     double num2 = 5.32984;
+     printf(" \n my num2 is = %lf", num2);
+
+     double division = num1 / num2;
+     printf(" \n the division of num1 and num2 is = %lf", division);
+
+
+
+    return 0;
+}
