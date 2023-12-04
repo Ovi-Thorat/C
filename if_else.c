559ea388047d75be3a3d929ddd1dd3f471e2babe
Ovi Thorat <151909288+Ovi-Thorat@users.noreply.github.com>
2023-12-04 09:45:43 +0530

    Create if_else.c

---
+++ if_else.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+  int main ()
+  {
+
+    char color = 'P';
+
+    if (color == 'Y') 
+    {
+        printf(" the color is pink");
+    } 
+    else{
+        printf(" the color is yellow");
+
+    }
+
+
+    return 0;
+  }
