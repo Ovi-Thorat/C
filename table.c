a92d1dc9230167830f7401bc2de5efdbb80684d0
Ovi Thorat <151909288+Ovi-Thorat@users.noreply.github.com>
2023-12-06 17:48:36 +0530

    Create table.c

---
+++ table.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int main()
+ {
+    int num, i;
+    printf("enter a number to generate the table in c:");
+    scanf("%d", &num);
+    
+    printf("\n table of %d", num);
+
+    return 0 ;
+   
+ }
