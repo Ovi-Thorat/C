0da0a227f52df2a7f1612f9fb65c7daabe1f7aba
Ovi Thorat <151909288+Ovi-Thorat@users.noreply.github.com>
2023-12-08 13:33:22 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ int main()
+ {
+    int num= 5;
+    int fact= 1;
+    int i;
+  
+    for(i=1; i<=5; i++ )
+    {
+        fact = fact * i;
+        
+       
+    }
+         printf("%d\n", fact);
+
+    return 0;
+ }
