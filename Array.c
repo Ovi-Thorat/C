885986cf50973a15b0d4bae2f5b9b5a23a0f7190
Ovi Thorat <151909288+Ovi-Thorat@users.noreply.github.com>
2024-01-12 12:06:12 +0530

    Create Array.c

---
+++ Array.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ int main()
+ {
+    int num[4] = {1,2,3,4}; 
+    // 1    2   3   4 --> elements/ values of number array
+    // 0    1   2   3 --> index
+    printf("%d %d  %d  %d", num[0], num[1], num[2], num[3] );
+     
+    return 0;
+
+ }
