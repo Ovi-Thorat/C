bfa21f74ab6ed04f4e21d9322d40a3712dd6c2c8
Ovi Thorat <151909288+Ovi-Thorat@users.noreply.github.com>
2024-01-13 18:26:05 +0530

    Create average.c

---
+++ average.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ int main()
+{
+    int average;
+    int num1, num2, num3;
+    printf(" /n enter the num1 :");
+    scanf("%d", &num1);
+
+    printf(" /n enter the num2 :");
+    scanf("%d", &num2);
+
+    printf(" /n enter the num3 :");
+    scanf("%d", &num3);
+
+    average = num1 + num2 + num3;
+    printf("enter the number of average= %d", average);
+
+    return 0;
+
+}
